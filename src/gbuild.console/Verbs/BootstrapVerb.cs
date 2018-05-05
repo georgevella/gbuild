8ee9b6c5dcc7764a3303207ec9c72cea690faad8
George Vella <mail@georgevella.com>
2018-05-05 17:57:12 +0200

    chore(console): prep for init and bootstrap commands

---
+++ src/gbuild.console/Verbs/BootstrapVerb.cs
@@ -0,0 +1,12 @@
+﻿using GBuild.Console.VerbOptions;
+
+namespace GBuild.Console.Verbs
+{
+    public class BootstrapVerb : IVerb<BootstrapOptions>
+    {
+        public void Run(BootstrapOptions options)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/gbuild.console/Verbs/InitializeVerb.cs
@@ -0,0 +1,12 @@
+﻿using GBuild.Console.VerbOptions;
+
+namespace GBuild.Console.Verbs
+{
+    public class InitializeVerb : IVerb<InitOptions>
+    {
+        public void Run(InitOptions options)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
